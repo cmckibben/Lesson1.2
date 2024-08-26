f53ed1430bb08d639b0bf61de18bbcc0118eb3f7
cmckibben <christopher_mckibben@hcpss.org>
2024-08-26 13:39:04 -0400

    Updated Instructions

--- src/main/java/org/example/Example1.java
+++ src/main/java/org/example/Example1.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Example1 {
     public static void main(String[] args) {
-        //Add an int, double, boolean, String, and char called score and print them
+        //Add an int, double, boolean, String, and char called score and print them in the form
+        //The score is 4!
 
     }
 }
